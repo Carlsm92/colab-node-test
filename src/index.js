fe4f56a460e8b8089595c5b9a55494a4f84609b4
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-06-28 20:33:07 +0200

    Added func sumaDeJero

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { saludoDeCarlos, saludoDeJero } from './functions/greeting.js'
+import { sumaDeJero } from './functions/sumaDeJero.js'
 
 const name = 'jERO'
 
 saludoDeCarlos(name)
 console.log(saludoDeJero(name))
 
-console.log(sumaDeCarlos(1, 2))
-console.log(sumaDeJero(10, 12))
+const num1 = 10
+const num2 = 12
+
+// console.log(sumaDeCarlos(1, 2))
+console.log(sumaDeJero(num1, num2))
