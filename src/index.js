1ecb31795efadbbde9e2db0ebf23fe497801b1ca
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-06-28 18:32:31 +0200

    Fixed error on instructions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 saludo(name) // <- Esta función debe devolver "Hola, 'nombre'"
 
-// La función nombre debe existir en la ruta src/functions/greeting.js
+// La función 'saludo' debe existir en la ruta src/functions/greeting.js
 // El parámetro name debe ser de tipo string
 // La función debe devolver un string con la primera letra del nombre en mayúsucla y el resto en minúscula
