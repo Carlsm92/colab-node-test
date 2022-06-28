e52fd1d91ffa704d789a09f52b1e57bfc17f3207
Carlos <carlos_sm92@hotmail.com>
2022-06-28 19:38:54 +0200

    Fixed saludo

--- src/functions/greeting.js
+++ src/functions/greeting.js
@@ -1,7 +1,7 @@
 export const saludo = (name) => {
   if (typeof name !== "number") {
-    console.log(`Hola, ${name}`)
     name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+    console.log(`Hola, ${name}`)
   } else 
     console.log('Introduce un dato NO numérico')
 }
